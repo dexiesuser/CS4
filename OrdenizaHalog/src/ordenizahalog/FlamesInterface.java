469fe687a4337cf16d0c83ef483547423910d47a
dex h <114088497+dexiesuser@users.noreply.github.com>
2024-02-28 22:41:59 +0800

    Add files via upload

---
+++ OrdenizaHalog/src/ordenizahalog/FlamesInterface.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ordenizahalog;
+
+/**
+ *
+ * @author dehal
+ */
+public interface FlamesInterface {
+
+    // Methods for getting input strings
+    public String getInputString1();
+    public String getInputString2();
+
+    // Method for analyzing the relationship and returning the result
+    public String analyzeRelationship();
+
+}
